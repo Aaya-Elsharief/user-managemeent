2fbe2bb6f8060061d16f6fef576d7172232f4e36
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-01 19:55:28 +0200

    create error handler related to #5

---
+++ server/utils/index.ts
@@ -0,0 +1,9 @@
+import userInterface from "./interfaces"
+import userValidation from "./validation"
+import CustomError from "./customErrors"
+
+export {
+    userValidation,
+    userInterface,
+    CustomError
+}
