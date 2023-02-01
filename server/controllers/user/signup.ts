240e24afeda700d4f07353c2002bdae9e04b2a2f
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-01 23:00:15 +0200

    user signup controller related to #5

--- server/controllers/user/signup.ts
+++ server/controllers/user/signup.ts
@@ -1,19 +1,31 @@
 import { Request, Response } from "express"
+import { hash } from "bcryptjs"
 import { userValidation, userInterface, CustomError } from "../../utils"
-
+import { checkUser, createUser} from "../../queries/"
 
 const signup = async(req: Request , res: Response) => {
     try{
         const {username, email, password, age }: userInterface = req.body
         await userValidation({username, email, password, age })
-        
-        //check validation in db 
-        //create new user in db
+             
+        const {emailCheck,usernameCheck } = await checkUser(username,email);
+        if (emailCheck) throw new CustomError(409, 'email already exist');
+        if (usernameCheck) throw new CustomError(409, 'username already exist');
 
-        res.send('Success')
+     
+        const hashedPassword = await hash(password, 12);
+
+        await createUser({username, email, password:hashedPassword, age} );
+        res.send('User Created Successfully')
+
     }catch (error) {
-        const customError = new CustomError(400, 'Invalid Inserted Data');
-        return res.status(customError.status).send({ message: customError.msg });    
+        if (error instanceof Error)
+            res.status(400).send({ message:  error.message });
+        else if (error instanceof CustomError) {
+          res.status(error.status).send({ message: error.message });
+        } else {
+          res.status(500).send({ message: 'internal server error' });
+        }
     }
 
 }
