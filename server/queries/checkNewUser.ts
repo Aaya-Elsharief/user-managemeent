841d307b1d275e90bb7de7bc32919b77ac8cb0e5
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-01 22:56:26 +0200

    make edits in customErrors related to #5

---
+++ server/queries/checkNewUser.ts
@@ -0,0 +1,18 @@
+import User from "../models";
+
+const checkUser = async(username:string, email:string) =>{
+    try{
+        const usernameCheck  = await User.find({username}).count();
+        const emailCheck  = await User.find({email}).count();
+
+
+        return Promise.resolve({emailCheck,usernameCheck} );
+      
+    } catch (error) {
+      return Promise.reject(error);
+    }
+
+}
+
+
+export default checkUser;
