9ab9248869366cdeb5d5bf51645797666e4c6e3b
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-01 19:54:42 +0200

    create userInterface related to #5

---
+++ server/utils/interfaces/userInterface.ts
@@ -0,0 +1,9 @@
+
+interface userInterface{
+    username: string | undefined,
+    email: string | undefined,
+    password: string | undefined,
+    age: number | undefined,
+}
+
+export default userInterface
