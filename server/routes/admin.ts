020926cf554791c3b8fc830dfdbc6ea86931d924
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-02 08:27:41 +0200

    create admin/users route related to #19

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { signup, login } from '../controllers';
-import userAuth from '../middlewares'
+import { signup, login, allUsers } from '../controllers';
+import {adminAuth} from '../middlewares'
 
 const adminRouter = Router();
 
 adminRouter.post('/signup', signup)
 adminRouter.post('/login', login)
+adminRouter.get('/users',adminAuth,  allUsers)
 
 export default adminRouter;
 
