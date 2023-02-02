d8fcee80c81a4dedbc10d6246e6318ea7f88f7f1
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-02 08:26:36 +0200

    create get allusers controller related to #19

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,9 +1,11 @@
 import { signup, login } from "./auth"
 import {profile, updateProfile} from "./user/"
+import allUsers from "./admin/allUsers"
 
 export {
     signup,
     login,
     profile,
     updateProfile,
+    allUsers
 }
