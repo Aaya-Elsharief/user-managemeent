fe9538fb47a87be1e5c02506f18e0972b3ee1864
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-02 05:32:13 +0200

    create admin router with other edits related to #15

--- server/middlewares/userAuth.ts
+++ server/middlewares/userAuth.ts
@@ -1,14 +1,14 @@
-import {  Response, NextFunction } from "express";
-import { CustomError, verifyToken} from "../utils"
+import { Response, NextFunction } from "express";
+import { CustomError, verifyToken } from "../utils"
 
-const userAuth = async(req: any, res:Response, next : NextFunction )=>{
-   try{
-        const token = req.headers.authorization?.replace('Bearer ', '');
-        if (!token) throw new CustomError(401, 'not authorize');
-        const verified = await verifyToken(token)
-        req.user= verified;
-        next();
-   }catch(error){
+const userAuth = async (req: any, res: Response, next: NextFunction) => {
+   try {
+      const token = req.headers.authorization?.replace('Bearer ', '');
+      if (!token) throw new CustomError(401, 'not authorize');
+      const verified = await verifyToken(token)
+      req.user = verified;
+      next();
+   } catch (error) {
       res.status(401).send({ message: 'not authorize' });
    }
 
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-import userRouter from './user'
+import userRouter from './user';
+import adminRouter from './admin';
 const router = Router();
 
+
 router.use('/user', userRouter)
+router.use('/admin', adminRouter)
 
 export default router;
