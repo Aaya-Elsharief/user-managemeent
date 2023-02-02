a22f4c1e209c79856b930e6df07d460f8fa07bde
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-02 03:48:01 +0200

    create userUpdate controller related to #13

--- server/controllers/user/signup.ts
+++ server/controllers/user/signup.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express"
 import { hash } from "bcryptjs"
 import { userValidation, userInterface, CustomError } from "../../utils"
-import { checkUser, createUser} from "../../queries/"
+import { checkUser, createUser } from "../../queries/"
 
-const signup = async(req: Request , res: Response) => {
-    try{
-        const {username, email, password, age }: userInterface = req.body
-        await userValidation({username, email, password, age })
-             
-        const {emailCheck,usernameCheck } = await checkUser(username,email);
-        if (emailCheck) throw new CustomError(409, 'email already exist');
-        if (usernameCheck) throw new CustomError(409, 'username already exist');
+const signup = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password, age }: userInterface = req.body
+    await userValidation(null, { username, email, password, age })
 
-     
-        const hashedPassword = await hash(password, 12);
+    const { emailCheck, usernameCheck } = await checkUser(username, email);
+    if (emailCheck) throw new CustomError(409, 'email already exist');
+    if (usernameCheck) throw new CustomError(409, 'username already exist');
 
-        await createUser({username, email, password:hashedPassword, age} );
-        res.send('User Created Successfully')
 
-    }catch (error) {
-        if (error instanceof Error)
-            res.status(400).send({ message:  error.message });
-        else if (error instanceof CustomError) {
-          res.status(error.status).send({ message: error.message });
-        } else {
-          res.status(500).send({ message: 'internal server error' });
-        }
+    const hashedPassword = await hash(password, 12);
+
+    await createUser({ username, email, password: hashedPassword, age });
+    res.send('User Created Successfully')
+
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(400).send({ message: error.message });
+    else if (error instanceof CustomError) {
+      res.status(error.status).send({ message: error.message });
+    } else {
+      res.status(500).send({ message: 'internal server error' });
     }
+  }
 
 }
 
---
+++ server/controllers/user/updateProfile.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import User from "../../models";
+import { userInterface, userValidation, CustomError } from "../../utils";
+
+const updateProfile = async (req: any, res: Response) => {
+    try {
+        const id = req.user.id;
+
+        const { username, email, age }: userInterface = req.body
+        await userValidation('upate', { username, email, age, password: "" })
+
+        const emailExist = await User.findOne({ email, _id: { $ne: id } });
+        const usernameExist = await User.findOne({ username, _id: { $ne: id } });
+        if (emailExist) throw new CustomError(409, 'email already exist');
+        if (usernameExist) throw new CustomError(409, 'username already exist');
+
+        await User.findByIdAndUpdate(id, {
+            $set: { username, email, age, updatedAt: Date.now() }
+        })
+
+        res.status(200).send('User profile updated successfully')
+
+    } catch (error) {
+        if (error instanceof Error)
+            res.status(400).send({ message: error.message });
+        else if (error instanceof CustomError) {
+            res.status(error.status).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: 'internal server error' });
+        }
+    }
+}
+
+
+export default updateProfile;
