ef1f8029c5161ae9a1840415349b0f81d1fc5f8c
Aaya-Elsharief <aaya.elsh@gmail.com>
2023-02-02 02:35:44 +0200

    create userAuth middleware to check guest authentication related to #9

---
+++ server/middlewares/userAuth.ts
@@ -0,0 +1,17 @@
+import {  Response, NextFunction } from "express";
+import { CustomError, verifyToken} from "../utils"
+
+const userAuth = async(req: any, res:Response, next : NextFunction )=>{
+   try{
+        const token = req.headers.authorization?.replace('Bearer ', '');
+        if (!token) throw new CustomError(401, 'not authorize');
+        const verified = await verifyToken(token)
+        req.user= verified;
+        next();
+   }catch(error){
+      res.status(401).send({ message: 'not authorize' });
+   }
+
+}
+
+export default userAuth;
